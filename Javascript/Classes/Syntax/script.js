841380e34a20839a1e7fc31418f7aae9052820d8
Abhay Agarwal <Abhay.agarwal@newput.com>
2024-04-10 14:11:22 +0530

    Classes

---
+++ Javascript/Classes/Syntax/script.js
@@ -0,0 +1,42 @@
+/**
+ * JavaScript classes are a powerful feature that simplifies and enhances object-oriented programming in JavaScript 
+ * 
+ * Classes provide a more familiar and structured way to create constructor functions and manage inheritance in JavaScript.
+ * 
+ * JavaScript's classes are still based on prototypes and constructor functions
+
+ * syntax
+ 
+class ClassName {
+  constructor( constructor parameters ) {
+    // Constructor function
+}
+
+method1( parameters ) {
+  // Method 1 definition
+}
+
+method2( parameters ) {
+  // Method 2 definition
+}
+
+// More methods...
+}
+
+*/
+
+/*
+Constructor:
+
+The constructor method is a special method used for initializing new objects created with the class.
+It's called automatically when you instantiate a new object using new ClassName().
+You can pass parameters to the constructor to initialize object properties.
+
+
+Methods:
+
+Methods are functions defined inside the class body.
+They define the behavior of class instances and can be called on instances created from the class.
+Methods can accept parameters and operate on the instance data using this.
+
+*/
