86dfdd69af8fb423dedc83ace922ebd7168a3c79
Abhay Agarwal <Abhay.agarwal@newput.com>
2024-04-10 14:14:27 +0530

    class syntax

--- Javascript/Classes/Syntax/script.js
+++ Javascript/Classes/Syntax/script.js
@@ -40,3 +40,35 @@ They define the behavior of class instances and can be called on instances creat
 Methods can accept parameters and operate on the instance data using this.
 
 */
+
+// Example of Using a Class:
+
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  greet() {
+    console.log(
+      `Hello, my name is ${this.name} and I am ${this.age} years old.`
+    );
+  }
+
+  celebrateBirthday() {
+    this.age++;
+    console.log(
+      `Happy birthday, ${this.name}! You are now ${this.age} years old.`
+    );
+  }
+}
+
+// Creating instances of the Person class
+const john = new Person("John", 30);
+const alice = new Person("Alice", 25);
+
+// Calling methods on class instances
+john.greet(); // Output: Hello, my name is John and I am 30 years old.
+alice.greet(); // Output: Hello, my name is Alice and I am 25 years old.
+
+john.celebrateBirthday(); // Output: Happy birthday, John! You are now 31 years old.
